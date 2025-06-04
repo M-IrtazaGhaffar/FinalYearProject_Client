2b6046dde0565fe6bcc0aacdd0665d82acf7044c
Muhammad Irtaza Ghaffar <mirtazaghaffar@gmail.com>
2025-06-04 11:11:34 +0500

    ui

--- app/dashboard/organization/blogs/page.jsx
+++ app/dashboard/organization/blogs/page.jsx
@@ -1,20 +1,6 @@
-"use client";
-import React, { useState } from 'react'
-import { useSession } from "next-auth/react";
+import React from 'react'
 
 function page() {
-  const [Data, setData] = useState('')
-  const session = useSession();
-
-  try {
-    // const response = organizationApi.post('https://advancedpos.duckdns.org/api/blog/getbyorganizationid', {
-    //   "organization_id": session?.user?.id
-    // })
-    // console.log(response);
-    
-  } catch (error) {
-    console.log(error);
-  }
   return (
     <div>page</div>
   )
